CyanogenMod/android_hardware_qcom_display

gralloc: allow forcing old pixel alignment
Since http://review.cyanogenmod.org/#/c/123901/, gralloc fallbacks
to an hard-coded pixel alignment for the NV12 and NV21 pixel formats
in case libadreno_utils doesn't export get_gpu_pixel_alignment.
However, it may happen that get_gpu_pixel_alignment is defined, but
the value it returns is not correct.

Allow targets to opt-out the use of the new adreno_utils api to
prevent alignment issues.

Change-Id: Ic8fb7880d236a96f8064d7deb8d1ef0567db39b7

libgralloc/alloc_controller.cpp
@@ -101,8 +101,10 @@ AdrenoMemInfo::AdrenoMemInfo()
             ::dlsym(libadreno_utils, "compute_aligned_width_and_height");
         *(void **)&LINK_adreno_compute_padding = ::dlsym(libadreno_utils,
                                            "compute_surface_padding");
+#ifndef ADRENO_FORCE_OLD_ALIGNMENT
         *(void **)&LINK_adreno_get_gpu_pixel_alignment =
                 ::dlsym(libadreno_utils, "get_gpu_pixel_alignment");
+#endif
     }
 }
 

YUTeleventures/hardware_qcom_display

Signed-off-by: AudioGod <audiogod@sungsonic.co>

libgralloc/alloc_controller.cpp
@@ -111,8 +111,10 @@ AdrenoMemInfo::AdrenoMemInfo()
         *(void **)&LINK_adreno_compute_compressedfmt_aligned_width_and_height =
                 ::dlsym(libadreno_utils,
                         "compute_compressedfmt_aligned_width_and_height");
+#ifndef DISABLE_GET_PIXEL_ALIGNMENT
         *(void **)&LINK_adreno_get_gpu_pixel_alignment =
                 ::dlsym(libadreno_utils, "get_gpu_pixel_alignment");
+#endif
     }
 }
 